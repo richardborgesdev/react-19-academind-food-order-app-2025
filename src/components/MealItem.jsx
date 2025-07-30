2198b9f7bdbbf9c65c5456f829ae5657b4dc9b61
richardborgesdev <richardborgesdev@gmail.com>
2025-07-30 09:40:11 -0300

    chore: UI | button component

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,4 +1,5 @@
 import * as formatting from "../util/formatting";
+import Button from "./UI/Button";
 
 export default function MealItem({ meal }) {
   return (
@@ -12,7 +13,7 @@ export default function MealItem({ meal }) {
           </p>
           <p className="meal-item-description">{meal.description}</p>
           <p className="meal-item-actions">
-            <button>Add to Cart</button>
+            <Button>Add to Cart</Button>
           </p>
         </div>
       </article>
