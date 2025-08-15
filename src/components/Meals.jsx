731f593e5d90e0aa6ed7cf7e352a855e17e1732a
richardborgesdev <richardborgesdev@gmail.com>
2025-08-15 09:06:20 -0300

    chore: custom hook | replace meals useEffect with useHttp

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,22 +1,18 @@
-import { useEffect, useState } from "react";
 import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
 
-export function Meals(params) {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+const requestConfig = {};
 
-  useEffect(() => {
-    async function fetchMeals(params) {
-      const response = await fetch("http://localhost:3000/meals");
+export function Meals() {
+  const {
+    data: loadedMeals,
+    isLoading,
+    error,
+  } = useHttp("http://localhost:3000/meals", requestConfig, []);
 
-      if (!response.ok) {
-      }
-
-      const resData = await response.json();
-      setLoadedMeals(resData);
-    }
-
-    fetchMeals();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching meals...</p>;
+  }
 
   return (
     <ul id="meals">
